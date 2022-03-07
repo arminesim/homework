c28ffda8e20b9412dd8021b192d980afdded2001
arminesim <101141817+arminesim@users.noreply.github.com>
2022-03-07 16:11:44 -0500

    Create task_6.js
    task 6

---
+++ homework 1/task_6.js
@@ -0,0 +1,3 @@
+let numMonth = +prompt ("Enter any number between 1 and 12");
+let month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November","December"];
+console.log(month[numMonth - 1]);
