0e99fc6c70915ad9f832d272783130182112c94a
arminesim <101141817+arminesim@users.noreply.github.com>
2022-03-07 15:51:26 -0500

    Create task_1.js
    task 1

---
+++ homework 1/task_1.js
@@ -0,0 +1,9 @@
+let input1 = +prompt ("Enter first number");
+let input2 = +prompt ("Enter second number");
+let a = Math.max(input1, input2);
+let b = Math.min(input1, input2);
+console.log (a+b);
+console.log (a-b);
+console.log (a*b);
+console.log (a/b);
+console.log (a%b);
